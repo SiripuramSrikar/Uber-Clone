99f94a6537ca23a9ac09241d0a4d627b7c7b6c1c
SiripuramSrikar <siripuramsrikar100705@gmail.com>
2024-12-30 15:31:52 +0530

    Header Completed

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,14 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist,Montserrat } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import Header from '../components/Header'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
+  subsets: ["latin"],  
 });
 
-const geistMono = Geist_Mono({
+const geistMono = Montserrat({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
@@ -18,12 +20,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+         <Header/>
         {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
